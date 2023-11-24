39755b5c62807d9090c92b49b8ed40bb260da013
NirmalSpikewell <k.nirmalkumar2002@gmail.com>
2023-11-25 03:58:27 +0530

    Terms & Conditons

--- app/src/main/java/com/bikie/in/Users/BookingSummary.java
+++ app/src/main/java/com/bikie/in/Users/BookingSummary.java
@@ -2,15 +2,42 @@ package com.bikie.in.Users;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
 
 import com.bikie.in.R;
 
 public class BookingSummary extends AppCompatActivity {
 
+    Button mProceedToPayment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_booking_summary);
+
+        mProceedToPayment = findViewById(R.id.btn_proceedToPayment);
+
+        mProceedToPayment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog = new Dialog(BookingSummary.this);
+
+                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+                dialog.setContentView(R.layout.terms_and_conditions);
+
+                dialog.show();
+                dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.getWindow().setWindowAnimations(R.style.BottomDialog);
+                dialog.getWindow().setGravity(Gravity.BOTTOM);
+            }
+        });
     }
 }
