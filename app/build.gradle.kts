ead2da92bd3558c59f09c8273e56c807f0afdb4b
NirmalSpikewell <k.nirmalkumar2002@gmail.com>
2024-01-10 01:46:02 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.bikie.in"
         minSdk = 33
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         android.buildFeatures.buildConfig = true
         buildConfigField("String", "PHONEPE_UAT_SALT", "\"${apiKeysProperties.getProperty("API_KEY_UAT_PHONEPE")}\"")
